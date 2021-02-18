affab9385d6c93d2c1ddc91317cef108ddf9b7fc
Sara Evans <saraevs@gmail.com>
2021-02-18 14:53:18 +0000

    generated doses controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # a user can see the list of cocktails
   get '/cocktails', to: 'cocktails#index'
+  get 'cocktails/:id', to: 'cocktails#show', as: 'cocktail'
 end
