be2f88df6382be69405d711426d1ff4489c05f38
Sara Evans <saraevs@gmail.com>
2021-02-18 16:32:55 +0000

    added the create controller action in the doses controller and the form to add new ingredients to a cocktail works

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,11 +8,18 @@ class DosesController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
+    @dose.save
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
