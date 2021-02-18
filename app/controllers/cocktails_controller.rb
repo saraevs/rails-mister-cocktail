7cb0e5e15ba5d1d93544abd0eb8fca9e46b9ad54
Sara Evans <saraevs@gmail.com>
2021-02-18 15:42:03 +0000

    added a link to the cocktails page to add new cocktail, and added the routes, actions and view to create a new cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -6,4 +6,25 @@ class CocktailsController < ApplicationController
   def show
     @cocktail = Cocktail.find(params[:id])
   end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+
+    if @cocktail.save
+      redirect_to cocktails_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
