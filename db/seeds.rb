95eda8ed9157994e2dfe5ccc16081d08fd91c651
Sara Evans <saraevs@gmail.com>
2021-02-18 13:00:11 +0000

    seeded the ingredients list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "passionfruit")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "rum")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "salt")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "lemon juice")
+Ingredient.create(name: "lime")
+Ingredient.create(name: "tonic")
