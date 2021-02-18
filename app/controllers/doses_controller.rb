d4e68ac952cd10edddabf8aee0e1919f2d0d26be
Sara Evans <saraevs@gmail.com>
2021-02-18 16:25:37 +0000

    added the routes, new controller action, and the simple form for adding an ingredient

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,18 @@
 class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get '/cocktails/new', to: 'cocktails#new', as: 'cocktail_new'
   get '/cocktails/:id', to: 'cocktails#show', as: 'cocktail'
   post '/cocktails', to: 'cocktails#create'
+
+  # a user can add a new dose
+  get 'cocktails/:cocktail_id/doses/new', to: 'doses#new', as: 'cocktail_dose_new'
+  post 'cocktails/:cocktail_id/doses', to: 'doses#create', as: 'cocktail_doses'
 end
